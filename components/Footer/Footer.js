a4d6c1b9fffa734d9ec714d5429d0dbdce4b5f9d
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2022-12-29 16:43:41 -0300

    add css footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
 import { BsGithub } from 'react-icons/bs'
+import styles from './Footer.module.css'
 
 export default function Footer() {
     return (
         <>
-            <div className='bg-bg flex h-16 items-center justify-center w-full text-gray-400'>
+            <div className={styles.footer}>
                 <p>Gabriel V. Pagnan</p>
-                <span className='text-white ml-5'>
+                <span>
                     <Link href='https://github.com/Gabriel-pagnan/DevCard' target='_blank'>
                         <BsGithub size={24} />
                     </Link>
